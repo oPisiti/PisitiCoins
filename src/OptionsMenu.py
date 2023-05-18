86bd4e968ec828e735bfc7a416d29bdb7e0c79ee
oPisiti <laurohrfranca@gmail.com>
2023-05-18 00:18:32 -0300

    Fixed Options Menu

--- src/OptionsMenu.py
+++ src/OptionsMenu.py
@@ -1,3 +1,4 @@
+import os
 from pynput import keyboard
 import subprocess
 from time import sleep
@@ -16,29 +17,31 @@ class bcolors:
     UPONELINE = '\033[F'
 
 
-# Keyboard
-KEYS_PRESSED = {
-    "up":    False,
-    "down":  False,
-    "enter": False
-}
-
-def update_keys_pressed(key) -> None:
+def update_keys_pressed(key = True) -> None:
     """ Updates the currently pressed keys' dictionary """
-    
-    global KEYS_PRESSED 
-    KEYS_PRESSED = {
-        "up":    key == keyboard.Key.up,
-        "down":  key == keyboard.Key.down,
-        "enter": key == keyboard.Key.enter
-    }
+
+    set_environ_bool("KEY_UP_IS_PRESSED",    key == keyboard.Key.up)
+    set_environ_bool("KEY_DOWN_IS_PRESSED",  key == keyboard.Key.down)
+    set_environ_bool("KEY_ENTER_IS_PRESSED", key == keyboard.Key.enter)
+    set_environ_bool("ANY_VALID_KEY_IS_PRESSED",   get_environ_bool("KEY_UP_IS_PRESSED") or get_environ_bool("KEY_DOWN_IS_PRESSED") or get_environ_bool("KEY_ENTER_IS_PRESSED"))
+
     pass
 
-# Adding listener for keyboard
-listener = keyboard.Listener(
-    suppress   = True,
-    on_press   = update_keys_pressed)
-listener.start()
+def set_environ_bool(var_str: str, value_bool: bool) -> None:
+    """ Sets an environment variable to a string equivalent to a boolean variable """
+    
+    os.environ[var_str] = "True" if value_bool else "False"
+
+
+def get_environ_bool(var_str: str) -> bool:
+    """ Returns a boolean equivalent to values of the strings 'True' and 'False' """
+
+    value = os.environ[var_str].lower()
+
+    match value:
+        case "true":  return True
+        case "false": return False
+        case _:       raise ValueError(f"Value {os.environ[var_str]} has no boolean equivalent") 
 
 
 def OptionsMenu(opt, greeting = "Choose an Option:"):
@@ -49,13 +52,21 @@ def OptionsMenu(opt, greeting = "Choose an Option:"):
     of what is on screen. Nothing other than what is written INSIDE this function gets rewritten
     """
 
-    global KEYS_PRESSED
+    # Determining the clear screen command based on the OS
+    clear_command = "cls" if os.name == "nt" else "clear" 
+
+    # Adding listener for keyboard
+    update_keys_pressed()
+    listener = keyboard.Listener(
+        suppress = True,
+        on_press = update_keys_pressed)
+    listener.start()
 
     chosen = False
     choice = 0
 
-
     while True:
+        os.system(clear_command)
 
         # Printing greeting and options
         print(greeting)
@@ -66,32 +77,40 @@ def OptionsMenu(opt, greeting = "Choose an Option:"):
             else:
                 print(opt[i]) 
 
+        # Waiting for a key to be pressed
+        while not get_environ_bool("ANY_VALID_KEY_IS_PRESSED"): 
+            sleep(0.1)
+
         # Handling key presses
-        if KEYS_PRESSED["up"]:                 
+        if get_environ_bool("KEY_UP_IS_PRESSED"):
             if choice > 0: choice -= 1 
             else:          choice = len(opt) - 1
             
-            KEYS_PRESSED["up"] = False  
+            set_environ_bool("KEY_UP_IS_PRESSED", False)
 
-        elif KEYS_PRESSED["down"]:
-            if choice < len(opt) - 1: choice +=1
-            else:                     choice = 0
+        elif get_environ_bool("KEY_DOWN_IS_PRESSED"):
+            if choice < len(opt) - 1: choice += 1
+            else:                     choice  = 0
 
-            KEYS_PRESSED["down"] = False  
+            set_environ_bool("KEY_DOWN_IS_PRESSED", False)
 
-        elif KEYS_PRESSED["enter"]: 
-            KEYS_PRESSED["enter"] = False
+        elif get_environ_bool("KEY_ENTER_IS_PRESSED"):
+            set_environ_bool("KEY_ENTER_IS_PRESSED", False)
 
             listener.stop()
             return opt[choice]
 
 
+        # Resetting the keys
+        update_keys_pressed()
+
         # Moving to the first line
         for i in range(len(opt) + 1):
             print("", end = bcolors.UPONELINE)  # Returns only the amount of lines used in this function
+        
 
-        # Preventing the screen to flick
-        sleep(0.1)
+    listener.stop()
+        
 
 if __name__ == "__main__":
     print("MY HUMBLE CODE")
