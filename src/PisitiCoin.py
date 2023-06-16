1919f364dbf61b12e66508c4876d16d522caeb48
oPisiti <laurohrfranca@gmail.com>
2023-06-15 23:28:10 -0300

    Pretty printing blocks

--- src/PisitiCoin.py
+++ src/PisitiCoin.py
@@ -3,6 +3,8 @@ import random
 import re
 import secrets
 
+from passlib.hash import pbkdf2_sha256
+
 from helper import *
 from OptionsMenu import *
 
@@ -12,6 +14,7 @@ class Globals:
     Global variables
     """
 
+    CLEAR_COMMAND = "cls" if os.name == "nt" else "clear"
     LOGGED_IN_AS = "None"
 
 
@@ -125,6 +128,38 @@ def get_interface_options() -> tuple:
     else:                            return logged_in
 
 
+def show_latest_blocks(db: BlockChain) -> None:
+    while True:
+        amount_blocks = input("How many blocks do you wish to see? ('a' for all) ")
+        
+        # If all
+        if amount_blocks.lower() == "a": 
+            amount_blocks = len(db.get_blocks_ids())
+
+        try:
+            amount_blocks = int(amount_blocks)
+            break
+        except ValueError as e:
+            pass
+
+    os.system(Globals.CLEAR_COMMAND)
+
+    count = 1
+    for block in db.get_all_blocks(id_order_asc=False):
+        print(f"Block {Colors.BOLD}#{block['id']}{Colors.ENDC}")                  
+        print(f"Previous Hash: {block['previous_hash']}")
+        print(f"From:          {block['from_id']}")
+        print(f"To:            {block['to_id']}")
+        print(f"Amount:        P$ {block['amount']}")
+        print(f"Mined By:      {block['miner_id']}")
+        print(f"Miner Reward:  P$ {block['miner_reward']}")
+        print(f"Nonce:         {block['nonce']}")
+        print(f"Hash:          {block['hash']}", end = "\r\n\n")
+
+        if count >= amount_blocks: break
+        count += 1
+
+
 def run_interface(db_path: str) -> None:
     """ Runs the main interface"""
 
@@ -135,9 +170,7 @@ def run_interface(db_path: str) -> None:
 
     options = get_interface_options()
     
-    # Determining the clear screen command based on the OS
-    clear_command = "cls" if os.name == "nt" else "clear" 
-    os.system(clear_command)
+    os.system(Globals.CLEAR_COMMAND)
 
     # Creating a connection to the database
     db = BlockChain(db_path)
@@ -146,144 +179,112 @@ def run_interface(db_path: str) -> None:
 
     answer = OptionsMenu(options, greeting)
 
-    if answer == "See Account Balance":
-        greeting = "Choose and account"        
-        answer = OptionsMenu(accounts_ids, greeting)
-
-        # accounts_ids is a tuple that contains username and id.
-        # Must filter only id
-        answer = extract_account_from_str(answer)
-        
-        acc_balance = db.get_account_balance(answer)
-        os.system(clear_command)
-        print(f"Account {accounts[answer]} ({answer}) has {acc_balance} PisitiCoins")
-    
-
-    elif answer == "Update All balances":
-        os.system(clear_command)
-        print("Updating balances for all users... ", end="")
-        db.update_all_balances()
-        print("Done!")
-
-
-    elif answer == "Send PisitiCoins":
-        while True:
-            greeting = "Choose your account"        
-            from_id = OptionsMenu(accounts_ids, greeting)
-            from_id = extract_account_from_str(from_id)
-
-            os.system(clear_command)
-
-            greeting = "Choose the account you wish to transfer to"        
-            to_id = OptionsMenu(accounts_ids, greeting)
-            to_id = extract_account_from_str(to_id)
-
-            os.system(clear_command)
-            
-            # Getting amount            
+    match answer:
+        case "Check Block Chain Validity":
             while True:
-                try:                    
-                    amount = float(input("How many PisitiCoins to transfer? "))
+                amount_blocks = input("How many blocks do you wish to verify? ('a' for all) ")
+                
+                # If all
+                if amount_blocks.lower() == "a": 
+                    amount_blocks = -1
+                
+                try:
+                    amount_blocks = int(amount_blocks)
                     break
-                except ValueError as e: 
-                    continue
-                finally:                
-                    os.system(clear_command)
+                except ValueError as e:
+                    pass
 
-            if amount < 0:
-                amount *= -1
+            os.system(Globals.CLEAR_COMMAND)
 
-            greeting = f"You wish to transfer {amount} PisitiCoins from {from_id} to {to_id}, correct?"
-            options = ["Yes", "No"]
-            if OptionsMenu(options, greeting) == "Yes":
-                break
+            error_on_block_id = db.check_chain_validity(amount_blocks)
+
+            if error_on_block_id is None: print("Blockchain is healthy")
+            else:                         print(f"Block with id {error_on_block_id} is broken")
+
+        case "Quit": raise StopIteration()
+
+        case "See Accounts Balances":
+            greeting = "Choose an account"        
+            answer = OptionsMenu(accounts_ids, greeting)
+
+            # accounts_ids is a tuple that contains username and id.
+            # Must filter only id
+            answer = extract_account_from_str(answer)
+            
+            acc_balance = db.get_account_balance(answer)
+            os.system(Globals.CLEAR_COMMAND)
+            print(f"Account {accounts[answer]} ({answer}) has {acc_balance} PisitiCoins")
         
-        # Getting random miner
-        miner_id = secrets.choice(tuple(db.get_accounts_ids_and_usernames().keys()))
-        block_data = {
-            "from_id":  from_id,
-            "to_id":    to_id,
-            "amount":   amount,
-            "miner_id": miner_id
-        }
+        case "Send PisitiCoins":
+            while True:
+                greeting = "Choose your account"        
+                from_id = OptionsMenu(accounts_ids, greeting)
+                from_id = extract_account_from_str(from_id)
 
-        try:
-            create_and_chain_block(db, block_data)
-            if os.environ["PRINT_STEPS"] == "True":
-                print("Transaction complete")
+                os.system(Globals.CLEAR_COMMAND)
 
-        except ValueError as e:         
-            from_balance = block_data["from_id"]   
-            print()
-            print(f"Insufficient funds in account {from_id}. Aborting operation")
-            print(f"Current balance: {db.get_account_balance(from_balance)} PisitiCoins")
+                greeting = "Choose the account you wish to transfer to"        
+                to_id = OptionsMenu(accounts_ids, greeting)
+                to_id = extract_account_from_str(to_id)
 
-        
-        # Updating the balances of accounts involved in transaction
-        for _id in (from_id, to_id, miner_id):
-            db.update_user_balance(_id)
+                os.system(Globals.CLEAR_COMMAND)
+                
+                # Getting amount            
+                while True:
+                    try:                    
+                        amount = float(input("How many PisitiCoins to transfer? "))
+                        break
+                    except ValueError as e: 
+                        continue
+                    finally:                
+                        os.system(Globals.CLEAR_COMMAND)
 
+                if amount < 0:
+                    amount *= -1
 
-    elif answer == "Check Block Chain Validity":
-        while True:
-            amount_blocks = input("How many blocks do you wish to verify? ('a' for all) ")
+                greeting = f"You wish to transfer {amount} PisitiCoins from {from_id} to {to_id}, correct?"
+                options = ["Yes", "No"]
+                if OptionsMenu(options, greeting) == "Yes":
+                    break
             
-            # If all
-            if amount_blocks.lower() == "a": 
-                amount_blocks = -1
-            
-            try:
-                amount_blocks = int(amount_blocks)
-                break
-            except ValueError as e:
-                pass
-
-        os.system(clear_command)
-
-        error_on_block_id = db.check_chain_validity(amount_blocks)
-
-        if error_on_block_id is None: print("Blockchain is healthy")
-        else:                         print(f"Block with id {error_on_block_id} is broken")
-
-
-    elif answer == "Show Latest blocks":
-        while True:
-            amount_blocks = input("How many blocks do you wish to see? ('a' for all) ")
-            
-            # If all
-            if amount_blocks.lower() == "a": 
-                amount_blocks = len(db.get_blocks_ids())
+            # Getting random miner
+            miner_id = secrets.choice(tuple(db.get_accounts_ids_and_usernames().keys()))
+            block_data = {
+                "from_id":  from_id,
+                "to_id":    to_id,
+                "amount":   amount,
+                "miner_id": miner_id
+            }
 
             try:
-                amount_blocks = int(amount_blocks)
-                break
-            except ValueError as e:
-                pass
+                create_and_chain_block(db, block_data)
+                if os.environ["PRINT_STEPS"] == "True":
+                    print("Transaction complete")
 
-        os.system(clear_command)
+            except ValueError as e:         
+                from_balance = block_data["from_id"]   
+                print()
+                print(f"Insufficient funds in account {from_id}. Aborting operation")
+                print(f"Current balance: {db.get_account_balance(from_balance)} PisitiCoins")
 
-        count = 1
-        for block in db.get_all_blocks(id_order_asc=False):
-            print(f"Block Number {block['id']}")                  
-            print(f"Previous Hash: {block['previous_hash']}")
-            print(f"From {block['from_id']}")
-            print(f"To {block['to_id']}")
-            print(f"Amount: {block['amount']} PisitiCoins")
-            print(f"Mined By {block['miner_id']}")
-            print(f"Miner Reward: {block['miner_reward']} PisitiCoins")
-            print(f"Nonce: {block['nonce']}")
-            print(f"Hash: {block['hash']}", end = "\r\n\n")
+            
+            # Updating the balances of accounts involved in transaction
+            for _id in (from_id, to_id, miner_id):
+                db.update_user_balance(_id)
 
-            if count >= amount_blocks: break
-            count += 1
+        case "Show Latest blocks":
+            show_latest_blocks(db)
 
-
-    elif answer == "Quit": raise StopIteration()
+        case "Update All balances":
+            os.system(Globals.CLEAR_COMMAND)
+            print("Updating balances for all users... ", end="")
+            db.update_all_balances()
+            print("Done!")
 
 
     input()
     input()
-    os.system(clear_command)
+    os.system(Globals.CLEAR_COMMAND)
 
 
 if __name__ == "__main__":
